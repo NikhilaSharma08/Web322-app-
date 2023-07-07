5e1302e1bbe0a55cf062ed15dc95c192904664ae
nikhila <134013390+NikhilaSharma08@users.noreply.github.com>
2023-07-07 11:25:24 -0400

    Update blog-service.js

--- blog-service.js
+++ blog-service.js
@@ -1,4 +1,32 @@
 
+/*********************************************************************************
+
+* WEB322 – Assignment 04
+
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+
+* No part of this assignment has been copied manually or electronically from any other source
+
+* (including web sites) or distributed to other students.
+
+*
+
+* Name: __________Nikhila Sharma____________ Student ID: _____168025211_________ Date:7/7/23 ________________
+
+*
+
+* Cyclic Web App URL: 
+https://rich-cyan-rattlesnake-fez.cyclic.app/about
+
+
+*
+
+* GitHub Repository URL: ____________https://github.com/NikhilaSharma08/Web322-app-_________________________________________
+
+*
+
+********************************************************************************/
+
 const fs = require('fs');
 const path = require("path");
 
